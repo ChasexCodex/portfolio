98b0006f643b6aa469e32af0ab9af43178bf4307
ElyasAmri <elyas.amri@gmail.com>
2022-01-13 17:31:27 +0300

    Project descriptions + main page improvements

--- pages/index.js
+++ pages/index.js
@@ -33,16 +33,17 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <hr className="m-4"/>
-      <p className="text-center text-3xl font-bold my-2">Projects</p>
+      <hr className="mx-4 mt-4 mb-1"/>
+      <p className="text-center text-3xl font-bold mb-4">Projects</p>
       <div className="mx-4 space-y-2 lg:grid grid-cols-2 lg:space-y-0 gap-2">
         {projects.map((p, i) => {
           return (
             <Link href={p.link ?? "/"} key={i}>
-              <a className="block rounded bg-white h-40 shadow-md p-4 flex">
-                <div className="flex-1">
+              <a className="block rounded bg-white h-40 shadow-md p-4 pb-2 flex">
+                <div className="flex-1 flex flex-col">
                   <p className="text-xl lg:text-2xl font-bold from-[#7C7A7AFF] to-[#7C7A7AFF] via-[#adadad] bg-gradient-from-t bg-gradient-to-b  w-max rounded-xl px-2">{p.name}</p>
-                  <p className="px-1 w-max">{p.from} - {p.to}</p>
+                  <p className="pl-1 leading-5 mt-0.5">{p.description}</p>
+                  <p className="px-1 text-center mt-auto text-sm bg-green-200 border border-gray-400 rounded-lg">{p.from} - {p.to}</p>
                 </div>
                 <Image src={p.image} className="rounded object-cover self-center" width={150} height={150}/>
               </a>
@@ -50,19 +51,20 @@ export default function Home() {
           )
         })}
       </div>
-      <hr className="m-4"/>
-      <p className="text-center text-3xl font-bold my-2">Contributions</p>
+      <hr className="mx-4 mt-4 mb-1"/>
+      <p className="text-center text-3xl font-bold mb-4">Contributions</p>
       <div className="mx-4 space-y-2 lg:grid grid-cols-4 lg:space-y-0 gap-2">
         {contributions.map((c, i) => {
           return (
-            <a href={c.url} target="_blank" rel="noreferrer" className="block bg-white rounded shadow-md p-4" key={i}>
+            <a href={c.url} target="_blank" rel="noreferrer" className="block bg-white rounded shadow-md px-4 py-2 lg:p-4" key={i}>
+              {c.image && <Image src={c.image} width={50} height={50}/>}
               <p>{c.name}: <span className="font-bold text-blue-400 underline">{c.href}</span></p>
             </a>
           )
         })}
       </div>
-      <hr className="m-4"/>
-      <p className="text-center text-3xl font-bold my-2">Social Links</p>
+      <hr className="mx-4 mt-4 mb-1"/>
+      <p className="text-center text-3xl font-bold mb-4">Social Links</p>
       <div className="mx-4 space-y-2 flex flex-col lg:flex-row lg:space-x-2 lg:h-40 lg:justify-center">
         {links.map((l, i) => {
           return (
