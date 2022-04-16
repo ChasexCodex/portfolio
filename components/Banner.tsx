71470ab9b170950ff7c13d10e85519467d57a4e8
ElyasAmri <elyas.amri@gmail.com>
2022-04-17 01:09:07 +0300

    Fix image resizing + image switch for mobile + helper

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import ShadyImage from './ShadyImage'
 
 export default function Banner() {
+  const image = <ShadyImage image1="/images/profile/normal.jpg"
+                            image2="/images/profile/ciphered.jpg"/>
   return (
       <>
         <div className="flex flex-col justify-center items-center mx-6 mt-4 space-y-4 md:hidden">
@@ -10,8 +12,7 @@ export default function Banner() {
             <p className="font-semibold">Technology-driven computer enthusiast with passion for music and coding</p>
           </div>
           <div className="flex w-max justify-center items-center border-2 rounded shadow">
-            <Image className="rounded aspect-ratio object-cover" src="/images/profile/normal.jpg" alt="Profile Photo"
-                   width="360" height="450"/>
+            {image}
           </div>
         </div>
         <div className="hidden md:flex flex-col justify-center items-center mt-4 mx-4 space-y-4">
@@ -20,12 +21,7 @@ export default function Banner() {
             <p className="font-semibold text-4xl lg:text-5xl leading-20 p-4 italic">
               Technology-driven computer enthusiast with passion for music and coding
             </p>
-            <div className="flex justify-center items-center rounded shadow">
-              <ShadyImage image1="/images/profile/normal.jpg"
-                          image2="/images/profile/ciphered.jpg"
-                          size={{width: 763, height: 900}}
-              />
-            </div>
+            {image}
           </div>
         </div>
       </>
--- components/ShadyImage.tsx
+++ components/ShadyImage.tsx
@@ -1,29 +1,24 @@
 import Image from 'next/image'
 import {PropsWithChildren} from 'react'
 
-type ImageSize = {
-  src: string
-}
-
 type Props = {
   image1: string
   image2: string
-  size: {
-    width: number
-    height: number
-  }
 }
 
-const ShadyImage = ({image1, image2, size, ...rest}: PropsWithChildren<Props>) => {
+const ShadyImage = ({image1, image2, ...rest}: PropsWithChildren<Props>) => {
   return (
-      <div style={{...size}} className="relative">
-        <div className="absolute top-0 left-0 ">
-          <Image src={image1} {...size} alt="Image 1"
-                 className="transition hover:opacity-0 duration-500 rounded-md"/>
+      <div className="flex-1 relative min-w-[350px] lg:min-w-[500px] h-[400px] lg:h-[600px]">
+        <div className="absolute inset-0">
+          <Image src={image1} alt="Image 1" layout="fill"
+                 className="object-cover transition hover:opacity-0 duration-500 rounded-md"/>
         </div>
-        <div className="absolute top-0 left-0 ">
-          <Image src={image2} {...size} alt="Image2"
-                 className="transition hover:opacity-100 duration-500 opacity-0 rounded-md"/>
+        <div className="absolute inset-0">
+          <Image src={image2} alt="Image2" layout="fill"
+                 className="object-cover transition hover:opacity-100 duration-500 opacity-0 rounded-md"/>
+        </div>
+        <div className="absolute top-2 right-2 bg-orange-400 rounded-xl px-1">
+          Click to switch
         </div>
       </div>
   )
