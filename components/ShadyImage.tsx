2b1219de0cbeaf026197558c02b8e011e79311a2
ElyasAmri <elyas.amri@gmail.com>
2022-04-17 18:15:03 +0300

    Additional improvements

--- components/ShadyImage.tsx
+++ components/ShadyImage.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
-import {PropsWithChildren} from 'react'
 
 type Props = {
   image1: string
   image2: string
 }
 
-const ShadyImage = ({image1, image2, ...rest}: PropsWithChildren<Props>) => {
+const ShadyImage = ({image1, image2}: Props) => {
   return (
-      <div className="flex-1 relative min-w-[350px] lg:min-w-[500px] h-[400px] lg:h-[600px]">
+      <div className="flex-1 relative
+      min-w-[350px] max-w-[350px] h-[400px]
+      lg:min-w-[500px] lg:max-w-[500px] lg:h-[600px]
+      ml-auto">
         <div className="absolute inset-0">
           <Image src={image1} alt="Image 1" layout="fill"
                  className="object-cover transition hover:opacity-0 duration-500 rounded-md"/>
@@ -17,8 +19,8 @@ const ShadyImage = ({image1, image2, ...rest}: PropsWithChildren<Props>) => {
           <Image src={image2} alt="Image2" layout="fill"
                  className="object-cover transition hover:opacity-100 duration-500 opacity-0 rounded-md"/>
         </div>
-        <div className="absolute top-2 right-2 bg-orange-400 rounded-xl px-1">
-          Click to switch
+        <div className="absolute animate-pulse text-sm top-2 right-2 bg-orange-400 rounded-xl px-1">
+          Click it to switch
         </div>
       </div>
   )
