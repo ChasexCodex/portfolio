81c1d29060e650aada5a0f53bdfde892d0639647
ElyasAmri <elyas.amri@gmail.com>
2022-04-19 02:08:44 +0300

    Bundle extras to components

--- components/Contributions.tsx
+++ components/Contributions.tsx
@@ -3,18 +3,24 @@ import Image from 'next/image'
 
 export default function Contributions() {
   return (
-      <div className="mx-4 space-y-2 lg:grid grid-cols-4 lg:space-y-0 gap-2">
-        {contributions.map((c, i) => {
-          return (
-              <a href={c.url} target="_blank" rel="noreferrer"
-                 className="block bg-white rounded shadow-md px-4 py-2 lg:p-4 dark:bg-gray-600 transform transition hover:scale-[102%] focus:scale-[102%]"
-                 key={i}>
-                {/*{c.image && <Image src={c.image} width={50} height={50}/>}*/}
-                <p><span className="dark:text-white">{c.name}:</span> <span
-                    className="font-bold text-blue-400 dark:text-blue-200 underline">{c.href}</span></p>
-              </a>
-          )
-        })}
+      <div>
+        <hr className="mx-4 mt-4 mb-1"/>
+        <p className="text-center text-3xl lg:text-5xl font-bold mb-2 lg:mb-4 dark:text-white">
+          Contributions
+        </p>
+        <div className="mx-4 space-y-2 lg:grid grid-cols-4 lg:space-y-0 gap-2">
+          {contributions.map((c, i) => {
+            return (
+                <a href={c.url} target="_blank" rel="noreferrer"
+                   className="block bg-white rounded shadow-md px-4 py-2 lg:p-4 dark:bg-gray-600 transform transition hover:scale-[102%] focus:scale-[102%]"
+                   key={i}>
+                  {/*{c.image && <Image src={c.image} width={50} height={50}/>}*/}
+                  <p><span className="dark:text-white">{c.name}:</span> <span
+                      className="font-bold text-blue-400 dark:text-blue-200 underline">{c.href}</span></p>
+                </a>
+            )
+          })}
+        </div>
       </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import {Banner, Contributions, Projects, Links} from '../components'
+import {Banner, Contributions, Projects, Links, Title} from '../components'
 
 export default function Home() {
   return (
@@ -9,31 +9,11 @@ export default function Home() {
         <title>Elyas A. Al-Amri - Leader of Tech</title>
         <meta name="description" content="The official website of Elyas A. Al-Amri"/>
       </Head>
-      <p className="font-bold px-4 text-4xl md:text-5xl lg:text-6xl dark:text-white">
-        Elyas A. Al-Amri - Leader of Tech
-      </p>
-      <div className="space-y-2 mx-4 mt-4">
-        <hr className="border-t-4 my-2"/>
-      </div>
 
+      <Title/>
       <Banner/>
-
-      <hr className="mx-4 mt-4 mb-1"/>
-      <p className="text-center text-3xl lg:text-5xl font-bold mb-2 lg:mb-4 dark:text-white">
-        Projects
-      </p>
       <Projects/>
-
-      <hr className="mx-4 mt-4 mb-1"/>
-      <p className="text-center text-3xl lg:text-5xl font-bold mb-2 lg:mb-4 dark:text-white">
-        Contributions
-      </p>
       <Contributions/>
-
-      <hr className="mx-4 mt-4 mb-1"/>
-      <p className="text-center text-3xl lg:text-5xl font-bold mb-2 lg:mb-4 dark:text-white">
-        Social Links
-      </p>
       <Links/>
     </>
   )
