7346c6e9f71406bcd1d387e9e7687619c1827a94
ElyasAmri <elyas.amri@gmail.com>
2022-06-03 19:22:54 +0300

    seo: add meta tags + open graph tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,17 @@ export default function Home() {
     <>
       <Head>
         <title>Elyas A. Al-Amri - Leader of Tech</title>
-        <meta name="description" content="The official website of Elyas A. Al-Amri"/>
+        <meta name="description" content="The official portfolio of Elyas A. Al-Amri"/>
+
+        <meta property="og:title" content="Elyas A. Al-Amri's Portfolio" key="og:title"/>
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_APP_URL}/website.jpg`} key="og:image"/>
+        <meta property="og:image:type" content="image/jpeg" key="og:image:type"/>
+        <meta property="og:image:width" content="1000" key="og:image:width"/>
+        <meta property="og:image:height" content="425" key="og:image:height"/>
+        <meta property="og:image:alt" content="Elyas with ambitous text" key="og:image:alt"/>
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_APP_URL} key="og:url"/>
+        <meta property="og:description" content="The best yet of Elyas A. Al-Amri" key="og:desc"/>
+        <meta property="og:type" content="website" key="og:type"/>
       </Head>
 
       <Title/>
