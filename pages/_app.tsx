988c1471564c77fd6e1152a6983f39f5cd57df0a
ElyasAmri <elyas.amri@gmail.com>
2022-06-03 22:13:13 +0300

    seo + ui: move meta tags to _app + remove link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,18 @@ export default function MyApp({Component, pageProps}: AppProps) {
       {/*<InteractiveBackground/>*/}
       <Head>
         <link rel="icon" href="/favicon.ico"/>
+        <title>Elyas A. Al-Amri - Leader of Tech</title>
+        <meta name="description" content="The official portfolio of Elyas A. Al-Amri"/>
+
+        <meta property="og:title" content="Elyas A. Al-Amri's Portfolio" key="og:title"/>
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_APP_URL}/website.jpg`} key="og:image"/>
+        <meta property="og:image:type" content="image/jpeg" key="og:image:type"/>
+        <meta property="og:image:width" content="1000" key="og:image:width"/>
+        <meta property="og:image:height" content="425" key="og:image:height"/>
+        <meta property="og:image:alt" content="Elyas with ambitous text" key="og:image:alt"/>
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_APP_URL} key="og:url"/>
+        <meta property="og:description" content="The best yet of Elyas A. Al-Amri" key="og:desc"/>
+        <meta property="og:type" content="website" key="og:type"/>
       </Head>
       <main className="z-10 flex-1 container mx-auto flex flex-col justify-center mt-4 py-2 lg:py-4">
         <Component {...pageProps} />
@@ -18,12 +30,10 @@ export default function MyApp({Component, pageProps}: AppProps) {
       <hr className="mt-4 mx-4 border-t-2"/>
       <footer className="w-max dark:bg-white px-2 py-1 rounded mx-auto my-6 flex space-x-4">
         <p className="font-semibold">Made by Elyas A. Al-Amri</p>
-        <a className="italic" href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-           target="_blank"
-           rel="noopener noreferrer">
+        <p className="italic">
           Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" className="transform translate-y-0.5" width={72} height={16}/>
-        </a>
+        </p>
       </footer>
       {process.env.useScreenSize === 'true' && <ScreenSize/>}
     </div>
